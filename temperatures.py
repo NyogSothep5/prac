ce59eb24ad3e6d69bb3fdb46dd3e4272b94849b3
NyogSothep5 <91268278+NyogSothep5@users.noreply.github.com>
2021-09-30 00:06:22 +0800

    Add files via upload

---
+++ temperatures.py
@@ -0,0 +1,32 @@
+"""
+CP1404/CP5632 - Practical
+Pseudocode for temperature conversion
+"""
+
+MENU = """C - Convert Celsius to Fahrenheit
+F - Convert Fahrenheit to Celsius
+Q - Quit"""
+print(MENU)
+choice = input(">>> ").upper()
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    celsius = (fahrenheit - 32) * 5 / 9.0
+    return celsius
+
+
+def celsius_to_fahrenheit(celsius):
+    fahrenheit = celsius * 9.0 / 5 + 32
+    return fahrenheit
+
+
+while choice != "Q":
+    if choice == "C":
+        celsius = float(input("celsius: "))
+        print(celsius_to_fahrenheit(celsius))
+    elif choice == "F":
+        fahrenheit = float(input("fahrenheit: "))
+        print(fahrenheit_to_celsius(fahrenheit))
+    else:
+        print("Invalid option")
+print("Thank you.")
